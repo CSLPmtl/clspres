f460e597b55da0c909310b9f167c5cec5f82c03e
Michael Hemingway <mpghemingway@gmail.com>
2018-10-22 16:03:30 -0400

    Resources Site Headless Site Initial commit

---
+++ src/components/nav/index.js
@@ -0,0 +1,25 @@
+/**
+ * This component renders the Global navigation
+ */
+import { Component } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style';
+
+export default class Nav extends Component {
+
+	state = {
+		image: { src: this.props.image, alt: '' }
+	}
+
+	render () {
+		return (
+			<nav>
+				{this.state.image.src !== null && <img src={this.state.image.src} alt="" />}
+				<p>{this.props.tool}</p>
+				<Link activeClassName={style.active} href="/">Home</Link>
+				<Link activeClassName={style.active} href="/profile">Me</Link>
+				<Link activeClassName={style.active} href="/profile/john">John</Link>
+			</nav>
+		);
+	}
+}
